d2181aa16b2f5892ac87ba99663ec000c0007b8b
moonyijeh2008 <163096793+moonyijeh2008@users.noreply.github.com>
2024-07-22 00:00:29 +0900

    finished Button.kt

--- app/src/main/java/com/tapie/eclair_card/MainActivity.kt
+++ app/src/main/java/com/tapie/eclair_card/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,6 +13,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.tapie.eclair_card.components.atomic.ButtonProps
+import com.tapie.eclair_card.components.atomic.CenteredTextButton
+import com.tapie.eclair_card.components.atomic.LeftTextRightIconButton
 import com.tapie.eclair_card.ui.theme.Eclair_cardTheme
 
 class MainActivity : ComponentActivity() {
@@ -42,6 +48,16 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
   Eclair_cardTheme {
-    Greeting("Android")
+
+    val buttonProps1 = ButtonProps(text = "타로", icon = null, buttonHeight = 60, buttonWidth = 250)
+    val buttonProps2 = ButtonProps(text = "타로 보러가기dfasfaf", icon = R.drawable.arrow_forward_ios, buttonHeight = 60, buttonWidth = 250)
+
+    Column(
+      modifier = Modifier.padding(16.dp),
+      verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+      CenteredTextButton(props = buttonProps1, onClick = { /* TODO: 버튼 클릭 시 동작 */ })
+      LeftTextRightIconButton(props = buttonProps2, onClick = { /* TODO: 버튼 클릭 시 동작 */ })
+    }
   }
 }
--- app/src/main/java/com/tapie/eclair_card/components/atomic/Button.kt
+++ app/src/main/java/com/tapie/eclair_card/components/atomic/Button.kt
@@ -1,13 +1,85 @@
 package com.tapie.eclair_card.components.atomic
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.TextStyle
+
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.tapie.eclair_card.R
+import com.tapie.eclair_card.ui.theme.GreenColor
 
 data class ButtonProps(
   val text: String,
-  val icon: String?
+  val icon: Int?,
+  val buttonHeight: Int,
+  val buttonWidth: Int
 )
 
-@Composable
-fun Button(props: TypographyProps) {
 
+@Composable
+fun CenteredTextButton(props: ButtonProps, onClick: () -> Unit) {
+  Button(
+    onClick = onClick,
+    modifier = Modifier
+      .padding(vertical = 4.dp)
+      .size(width = props.buttonWidth.dp, height = props.buttonHeight.dp),
+    colors = ButtonDefaults.buttonColors(containerColor = GreenColor),
+    shape = RoundedCornerShape(8.dp)
+  ) {
+    Text(
+      text = props.text,
+      style = TextStyle(
+        color = Color.White,
+        fontSize = 16.sp
+      )
+    )
+  }
+}
+
+@Composable
+fun LeftTextRightIconButton(props: ButtonProps, onClick: () -> Unit) {
+  Button(
+    onClick = onClick,
+    modifier = Modifier
+      .padding(vertical = 4.dp)
+      .size(width = props.buttonWidth.dp, height = props.buttonHeight.dp),
+    colors = ButtonDefaults.buttonColors(containerColor = GreenColor),
+    shape = RoundedCornerShape(8.dp)
+  ) {
+    Row(
+      modifier = Modifier.fillMaxWidth(),
+      horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+      Text(
+        text = props.text,
+        style = TextStyle(
+          color = Color.Black,
+          fontSize = 16.sp
+        )
+      )
+      props.icon?.let {
+        Image(
+          painter = painterResource(id = it),
+          contentDescription = null
+        )
+      }
+    }
+  }
 }
