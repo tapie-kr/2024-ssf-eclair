f4db249066db94d6a98ea68ded8032d9a5da57e4
moonyijeh2008 <163096793+moonyijeh2008@users.noreply.github.com>
2024-07-22 21:06:59 +0900

    typography
    typography

--- app/src/main/java/com/tapie/eclair_card/components/atomic/Typography.kt
+++ app/src/main/java/com/tapie/eclair_card/components/atomic/Typography.kt
@@ -1,13 +1,33 @@
 package com.tapie.eclair_card.components.atomic
 
-import androidx.compose.runtime.Composable
 
-data class TypographyProps(
-  val text: String,
-  val icon: String?
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.tapie.eclair_card.R
+
+
+val Pretendard = FontFamily(
+  Font(R.font.pretendard_font_400, FontWeight.W400),
+  Font(R.font.pretendard_font_500, FontWeight.W500),
+  Font(R.font.pretendard_font_600, FontWeight.W600),
+  Font(R.font.pretendard_font_700, FontWeight.W700)
 )
 
-@Composable
-fun Typography(props: TypographyProps) {
-
-}
+// 다양한 텍스트 스타일 정의
+val AppTypography = Typography(
+  bodyLarge = TextStyle(
+    fontFamily = Pretendard,
+    fontWeight = FontWeight.Normal,
+    fontSize = 16.sp
+  ),
+  labelLarge = TextStyle(
+    fontFamily = Pretendard,
+    fontWeight = FontWeight.Bold,
+    fontSize = 14.sp
+  ),
+  // 추가 텍스트 스타일 설정
+)
