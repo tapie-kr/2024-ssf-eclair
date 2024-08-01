8254ea03d88b83fb7bdf48ae109dfbc0e66984c5
moonyijeh2008 <163096793+moonyijeh2008@users.noreply.github.com>
2024-08-02 01:22:26 +0900

    file plus

--- app/src/main/java/com/tapie/eclair_card/components/nav/BottomNavigation.kt
+++ app/src/main/java/com/tapie/eclair_card/components/nav/BottomNavigation.kt
@@ -1,7 +1,3 @@
 package com.tapie.eclair_card.components.nav
 
 
-
-
-
-
--- app/src/main/java/com/tapie/eclair_card/fragment/starting/StartingScreen.kt
+++ app/src/main/java/com/tapie/eclair_card/fragment/starting/StartingScreen.kt
@@ -1,2 +1,34 @@
 package com.tapie.eclair_card.fragment.starting
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+
+@Composable
+fun StartingScreen(navController: NavController) {
+    var name by remember { mutableStateOf(TextFieldValue("")) }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = "당신의 이름을 알려주세요.",
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = Modifier.padding(bottom = 8.dp)
+        )
+
+    }
+}
