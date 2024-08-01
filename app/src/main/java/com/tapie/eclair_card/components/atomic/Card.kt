5d109d203741574d22abba0c9fa8870d1743c1d4
moonyijeh2008 <163096793+moonyijeh2008@users.noreply.github.com>
2024-08-01 18:38:33 +0900

    Card

--- app/src/main/java/com/tapie/eclair_card/components/atomic/Card.kt
+++ app/src/main/java/com/tapie/eclair_card/components/atomic/Card.kt
@@ -1,2 +1,222 @@
 package com.tapie.eclair_card.components.atomic
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.tapie.eclair_card.R
+
+@Composable
+fun CardBack(modifier: Modifier = Modifier){
+    Image(
+        painter = painterResource(id = R.drawable.card_back),
+        contentDescription = null,
+        modifier = modifier
+    )
+}
+
+@Composable
+fun Card_0(modifier: Modifier = Modifier){
+    Image(
+        painter = painterResource(id = R.drawable.card_0),
+        contentDescription = null,
+        modifier = modifier
+    )
+}
+
+@Composable
+fun Card_1(modifier: Modifier = Modifier){
+    Image(
+        painter = painterResource(id = R.drawable.card_1),
+        contentDescription = null,
+        modifier = modifier
+    )
+}
+
+@Composable
+fun Card_2(modifier: Modifier = Modifier){
+    Image(
+        painter = painterResource(id = R.drawable.card_2),
+        contentDescription = null,
+        modifier = modifier
+    )
+}
+
+@Composable
+fun Card_3(modifier: Modifier = Modifier){
+    Image(
+        painter = painterResource(id = R.drawable.card_3),
+        contentDescription = null,
+        modifier = modifier
+    )
+}
+
+@Composable
+fun Card_4(modifier: Modifier = Modifier){
+    Image(
+        painter = painterResource(id = R.drawable.card_4),
+        contentDescription = null,
+        modifier = modifier
+    )
+}
+
+@Composable
+fun Card_5(modifier: Modifier = Modifier){
+    Image(
+        painter = painterResource(id = R.drawable.card_5),
+        contentDescription = null,
+        modifier = modifier
+    )
+}
+
+@Composable
+fun Card_6(modifier: Modifier = Modifier){
+    Image(
+        painter = painterResource(id = R.drawable.card_6),
+        contentDescription = null,
+        modifier = modifier
+    )
+}
+
+@Composable
+fun Card_7(modifier: Modifier = Modifier){
+    Image(
+        painter = painterResource(id = R.drawable.card_7),
+        contentDescription = null,
+        modifier = modifier
+    )
+}
+
+@Composable
+fun Card_8(modifier: Modifier = Modifier){
+    Image(
+        painter = painterResource(id = R.drawable.card_8),
+        contentDescription = null,
+        modifier = modifier
+    )
+}
+
+@Composable
+fun Card_9(modifier: Modifier = Modifier){
+    Image(
+        painter = painterResource(id = R.drawable.card_9),
+        contentDescription = null,
+        modifier = modifier
+    )
+}
+
+@Composable
+fun Card_10(modifier: Modifier = Modifier){
+    Image(
+        painter = painterResource(id = R.drawable.card_10),
+        contentDescription = null,
+        modifier = modifier
+    )
+}
+
+@Composable
+fun Card_11(modifier: Modifier = Modifier){
+    Image(
+        painter = painterResource(id = R.drawable.card_11),
+        contentDescription = null,
+        modifier = modifier
+    )
+}
+
+@Composable
+fun Card_12(modifier: Modifier = Modifier){
+    Image(
+        painter = painterResource(id = R.drawable.card_12),
+        contentDescription = null,
+        modifier = modifier
+    )
+}
+
+@Composable
+fun Card_13(modifier: Modifier = Modifier){
+    Image(
+        painter = painterResource(id = R.drawable.card_13),
+        contentDescription = null,
+        modifier = modifier
+    )
+}
+
+@Composable
+fun Card_14(modifier: Modifier = Modifier){
+    Image(
+        painter = painterResource(id = R.drawable.card_14),
+        contentDescription = null,
+        modifier = modifier
+    )
+}
+
+@Composable
+fun Card_15(modifier: Modifier = Modifier){
+    Image(
+        painter = painterResource(id = R.drawable.card_15),
+        contentDescription = null,
+        modifier = modifier
+    )
+}
+
+@Composable
+fun Card_16(modifier: Modifier = Modifier){
+    Image(
+        painter = painterResource(id = R.drawable.card_16),
+        contentDescription = null,
+        modifier = modifier
+    )
+}
+
+@Composable
+fun Card_17(modifier: Modifier = Modifier){
+    Image(
+        painter = painterResource(id = R.drawable.card_17),
+        contentDescription = null,
+        modifier = modifier
+    )
+}
+
+@Composable
+fun Card_18(modifier: Modifier = Modifier){
+    Image(
+        painter = painterResource(id = R.drawable.card_18),
+        contentDescription = null,
+        modifier = modifier
+    )
+}
+
+@Composable
+fun Card_19(modifier: Modifier = Modifier){
+    Image(
+        painter = painterResource(id = R.drawable.card_19),
+        contentDescription = null,
+        modifier = modifier
+    )
+}
+
+@Composable
+fun Card_20(modifier: Modifier = Modifier){
+    Image(
+        painter = painterResource(id = R.drawable.card_20),
+        contentDescription = null,
+        modifier = modifier
+    )
+}
+
+@Composable
+fun Card_21(modifier: Modifier = Modifier){
+    Image(
+        painter = painterResource(id = R.drawable.card_21),
+        contentDescription = null,
+        modifier = modifier
+    )
+}
+
